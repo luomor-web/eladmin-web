9418de08bf738da5adf7dcf6463b2969af65cbd7
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-13 16:38:27 +0800

    update

--- src/store/modules/api.js
+++ src/store/modules/api.js
@@ -3,21 +3,21 @@ const baseUrl = process.env.VUE_APP_BASE_API === '/' ? '' : process.env.VUE_APP_
 const api = {
   state: {
     // 部署包上传
-    deployUploadApi: baseUrl + '/api/deploy/upload',
+    deployUploadApi: baseUrl + '/bsc/deploy/upload',
     // SQL脚本上传
-    databaseUploadApi: baseUrl + '/api/database/upload',
+    databaseUploadApi: baseUrl + '/bsc/database/upload',
     // 图片上传
-    imagesUploadApi: baseUrl + '/api/localStorage/pictures',
+    imagesUploadApi: baseUrl + '/bsc/localStorage/pictures',
     // 修改头像
-    updateAvatarApi: baseUrl + '/api/users/updateAvatar',
+    updateAvatarApi: baseUrl + '/bsc/users/updateAvatar',
     // 上传文件到七牛云
-    qiNiuUploadApi: baseUrl + '/api/qiNiuContent',
+    qiNiuUploadApi: baseUrl + '/bsc/qiNiuContent',
     // Sql 监控
     sqlApi: baseUrl + '/druid/index.html',
     // swagger
     swaggerApi: baseUrl + '/doc.html',
     // 文件上传
-    fileUploadApi: baseUrl + '/api/localStorage',
+    fileUploadApi: baseUrl + '/bsc/localStorage',
     // baseUrl，
     baseApi: baseUrl
   }
